9b7af1635bf7cef51791f908dc37b2dbf7544bab
edercirino <edersantoscirino@gmail.com>
2025-05-08 11:40:52 -0300

    Create: Private routes component

--- src/auth/AuthContext.jsx
+++ src/auth/AuthContext.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const AuthContext = React.createContext();
 
+// eslint-disable-next-line react-refresh/only-export-components
 export function useAuth() {
   return React.useContext(AuthContext);
 }
