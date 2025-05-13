5a0536bd1fff5d33e7b7cacbd510f537814461cd
edercirino <edersantoscirino@gmail.com>
2025-05-13 20:38:17 -0300

    Style: Button logout

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,12 +1,24 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Navigate, NavLink, useNavigate } from "react-router-dom";
 import { ListChecks, User, LogOut } from "lucide-react";
+import { useAuth } from "../auth/AuthContext";
 
 const SideBar = () => {
   const navLinkClass = ({ isActive }) =>
     `flex items-center gap-2 px-4 py-2 rounded text-white hover:bg-gray-500 transition ${
       isActive ? "bg-gray-400 font-semibold" : "text-gray-700"
     }`;
+
+  const navButtonClass =
+    "flex items-center gap-2 px-4 py-2 rounded text-white hover-gray-500 transition w-full";
+
+  const navigate = useNavigate();
+  const { logout } = useAuth();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
 
   return (
     <aside className="2-64  border-r bg-blue-500 h-full p-4">
@@ -23,10 +35,10 @@ const SideBar = () => {
           Profile
         </NavLink>
 
-        <NavLink to="/login" className={navLinkClass}>
+        <button onClick={handleLogout} className={navButtonClass}>
           <LogOut size={18} />
           Logout
-        </NavLink>
+        </button>
       </nav>
     </aside>
   );
