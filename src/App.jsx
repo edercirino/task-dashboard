3436a79d80dac8b7af8b9d7a57a65f46cfa81d80
edercirino <edersantoscirino@gmail.com>
2025-05-22 21:04:06 -0300

    Update: Add toat notification

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const App = () => {
-  return <div>Task Dashboard</div>;
-};
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import { AuthProvider } from "./auth/AuthContext.jsx";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
       <BrowserRouter>
         <AppRoutes />
+        <ToastContainer />
       </BrowserRouter>
     </AuthProvider>
   </StrictMode>
